85046e5d14be5d3b806d393398c2517515e191af
Quang Vinh <typhutnt@gmail.com>
2025-10-31 21:51:25 +0900

    Update banner.js

--- data/banner.js
+++ data/banner.js
@@ -1,16 +1,17 @@
 //data/banner.js
 // --- DỮ LIỆU BANNER (Giữ nguyên, không cần gộp vào allContent) ---
 const bannerSlides = [
-    {
-        imageUrl: "/img/banners/1020.jpg",
-        link: "#"
-    },
+    
     {
         imageUrl: "/img/banners/1120.jpg",
         link: "#"
     },
     {
         imageUrl: "/img/banners/1222.jpg",
+        link: "#"
+    },
+    {
+        imageUrl: "/img/banners/happy-new-year-2026.jpg",
         link: "#"
     }
 ];
