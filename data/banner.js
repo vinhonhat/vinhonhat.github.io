c026b766ead0e84a83a1711fef8b31a2e145950e
Quang Vinh <typhutnt@gmail.com>
2025-10-31 10:51:56 +0900

    Create banner.js

---
+++ data/banner.js
@@ -0,0 +1,16 @@
+//data/banner.js
+// --- DỮ LIỆU BANNER (Giữ nguyên, không cần gộp vào allContent) ---
+const bannerSlides = [
+    {
+        imageUrl: "/img/banners/1020.jpg",
+        link: "#"
+    },
+    {
+        imageUrl: "/img/banners/1120.jpg",
+        link: "#"
+    },
+    {
+        imageUrl: "/img/banners/1222.jpg",
+        link: "#"
+    }
+];
