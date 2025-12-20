43d9044742696683cf320e6d5ea9933e889c21a3
Quang Vinh <typhutnt@gmail.com>
2025-12-21 08:30:00 +0900

    Update holidays.js

--- js/holidays.js
+++ js/holidays.js
@@ -7,12 +7,12 @@ const holidays = [
     { day: 8, month: 3, isLunar: false, name: "Quốc tế Phụ nữ", imagePrefix: "0308" },
     { day: 30, month: 4, isLunar: false, name: "Giải phóng miền Nam 30/4 và Quốc tế Lao Động 1/5", imagePrefix: "0430" },
     //{ day: 1, month: 5, isLunar: false, name: "Quốc tế Lao động", imagePrefix: "0501" },
-    { day: 19, month: 5, isLunar: false, name: "Ngày sinh Chủ tịch Hồ Chí Minh", imagePrefix: "0519" },
+    { day: 19, month: 5, isLunar: false, name: "sinh Chủ tịch Hồ Chí Minh", imagePrefix: "0519" },
     { day: 1, month: 6, isLunar: false, name: "Quốc tế Thiếu nhi", imagePrefix: "0601" },
     { day: 2, month: 9, isLunar: false, name: "Quốc khánh Việt Nam", imagePrefix: "0902" },
-    { day: 20, month: 10, isLunar: false, name: "Ngày Phụ nữ Việt Nam", imagePrefix: "1020" },
-    { day: 20, month: 11, isLunar: false, name: "Ngày Nhà giáo Việt Nam", imagePrefix: "1120" },
-    { day: 22, month: 12, isLunar: false, name: "Ngày Thành Lập Quân Đội Nhân Dân Việt Nam", imagePrefix: "1222" },
+    { day: 20, month: 10, isLunar: false, name: "Phụ nữ Việt Nam", imagePrefix: "1020" },
+    { day: 20, month: 11, isLunar: false, name: "Nhà giáo Việt Nam", imagePrefix: "1120" },
+    { day: 22, month: 12, isLunar: false, name: "Thành Lập Quân Đội Nhân Dân Việt Nam", imagePrefix: "1222" },
     { day: 25, month: 12, isLunar: false, name: "Lễ Giáng Sinh", imagePrefix: "1225" },
 
 ];
