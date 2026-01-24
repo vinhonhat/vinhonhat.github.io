3349e16c323bda2376f8dccb071daeaebb27a6fa
Quang Vinh <typhutnt@gmail.com>
2026-01-24 18:37:31 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -783,7 +783,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
                         setTimeout(() => {
                             window.location.reload(true);
-                        }, 1000); 
+                        }, 2000); 
 
                     } catch (error) {
                         console.error("Lỗi khi xóa cache:", error);
