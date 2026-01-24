b8c525ecc74aa72baa8ba352bb64b45272fce6d0
Quang Vinh <typhutnt@gmail.com>
2026-01-24 15:00:22 +0900

    Update holidays.js

--- js/holidays.js
+++ js/holidays.js
@@ -1,18 +1,30 @@
 const holidays = [
-    { day: 1, month: 1, isLunar: true, name: "Tết Nguyên Đán", imagePrefix: "tet" },
-    { day: 10, month: 3, isLunar: true, name: "Giỗ Tổ Hùng Vương", imagePrefix: "hungvuong" },
-    { day: 15, month: 8, isLunar: true, name: "Tết Trung Thu", imagePrefix: "trungthu" },
+    // ---------------- NGÀY DƯƠNG LỊCH -----------------
     { day: 1, month: 1, isLunar: false, name: "Tết Dương Lịch", imagePrefix: "0101" },
+    { day: 3, month: 2, isLunar: false, name: "Thành lập Đảng Cộng Sản Việt Nam ", imagePrefix: "0203" },
     { day: 14, month: 2, isLunar: false, name: "Lễ Tình Nhân", imagePrefix: "0214" },
+    { day: 27, month: 2, isLunar: false, name: "Thầy thuốc Việt Nam", imagePrefix: "0227" },
     { day: 8, month: 3, isLunar: false, name: "Quốc tế Phụ nữ", imagePrefix: "0308" },
+    { day: 26, month: 3, isLunar: false, name: "Thành lập Đoàn TNCS Hồ Chí Minh ", imagePrefix: "0308" },
     { day: 30, month: 4, isLunar: false, name: "Giải phóng miền Nam 30/4 và Quốc tế Lao Động 1/5", imagePrefix: "0430" },
-    //{ day: 1, month: 5, isLunar: false, name: "Quốc tế Lao động", imagePrefix: "0501" },
     { day: 19, month: 5, isLunar: false, name: "sinh Chủ tịch Hồ Chí Minh", imagePrefix: "0519" },
     { day: 1, month: 6, isLunar: false, name: "Quốc tế Thiếu nhi", imagePrefix: "0601" },
     { day: 2, month: 9, isLunar: false, name: "Quốc khánh Việt Nam", imagePrefix: "0902" },
     { day: 20, month: 10, isLunar: false, name: "Phụ nữ Việt Nam", imagePrefix: "1020" },
+    { day: 9, month: 11, isLunar: false, name: " Quốc tế Nam giới", imagePrefix: "1109" },
     { day: 20, month: 11, isLunar: false, name: "Nhà giáo Việt Nam", imagePrefix: "1120" },
+    { day: 24, month: 11, isLunar: false, name: "Văn hóa Việt Nam", imagePrefix: "1124" },
     { day: 22, month: 12, isLunar: false, name: "Thành Lập Quân Đội Nhân Dân Việt Nam", imagePrefix: "1222" },
     { day: 24, month: 12, isLunar: false, name: "Lễ Giáng Sinh", imagePrefix: "1224" },
+    // ---------------- NGÀY ÂM LỊCH -----------------
+    { day: 1, month: 1, isLunar: true, name: "Tết Nguyên Đán", imagePrefix: "tet" },
+    { day: 10, month: 3, isLunar: true, name: "Giỗ Tổ Hùng Vương", imagePrefix: "hungvuong" },
+    { day: 15, month: 8, isLunar: true, name: "Tết Trung Thu", imagePrefix: "trungthu" },
 
 ];
+    //{ day: 09, month: 11, isLunar: false, name: " Quốc tế Nam giới", imagePrefix: "1109" },
+    
+    
+    
+
+    
