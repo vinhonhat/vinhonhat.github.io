49c4309a5937902e1b37359d610ef18700555425
Quang Vinh <typhutnt@gmail.com>
2026-03-23 17:08:29 +0900

    1

--- js/holidays.js
+++ js/holidays.js
@@ -4,28 +4,28 @@ const holidays = [
     { day: 1, month: 1, isLunar: true, name: "Tết Nguyên Đán", imagePrefix: "tet" },    //                   chưa                                        // 2026
     { day: 3, month: 2, isLunar: false, name: "Thành lập Đảng Cộng Sản Việt Nam ", imagePrefix: "0203" }, // chưa ------------------
     //{ day: 14, month: 2, isLunar: false, name: "Lễ Tình Nhân", imagePrefix: "0214" },                                       //ok
-    { day: 27, month: 2, isLunar: false, name: "Thầy thuốc Việt Nam", imagePrefix: "0227" },                                //ok
-    { day: 8, month: 3, isLunar: false, name: "Quốc tế Phụ nữ", imagePrefix: "0308" },                                      //ok
+    { day: 27, month: 2, isLunar: false, name: "Thầy thuốc Việt Nam", imagePrefix: "0227" },                              //ok
+    { day: 8, month: 3, isLunar: false, name: "Quốc tế Phụ nữ", imagePrefix: "0308" },                                    //ok
 
-    { day: 10, month: 3, isLunar: true, name: "Giỗ Tổ Hùng Vương", imagePrefix: "0310" },// ÂM LỊCH
+    { day: 10, month: 3, isLunar: true, name: "Giỗ Tổ Hùng Vương", imagePrefix: "0310" },// ÂM LỊCH                       //ok
 
-    { day: 26, month: 3, isLunar: false, name: "Thành lập Đoàn TNCS Hồ Chí Minh ", imagePrefix: "0326" },                   //ok
-    { day: 30, month: 4, isLunar: false, name: "Giải phóng miền Nam 30/4 và Quốc tế Lao Động 1/5", imagePrefix: "04300501" },   //
+    { day: 26, month: 3, isLunar: false, name: "Thành lập Đoàn TNCS Hồ Chí Minh ", imagePrefix: "0326" },                 //ok
+    { day: 30, month: 4, isLunar: false, name: "Giải phóng miền Nam 30/4 và Quốc tế Lao Động 1/5", imagePrefix: "04300501" }, // ok
     //{ day: 30, month: 4, isLunar: false, name: "Giải phóng miền Nam 30/4 Thống nhất đất nước", imagePrefix: "0430" },   //
     //{ day: 01, month: 5, isLunar: false, name: "Quốc tế Lao Động 1/5", imagePrefix: "0501" },   //    
-    { day: 19, month: 5, isLunar: false, name: "sinh Chủ tịch Hồ Chí Minh", imagePrefix: "0519" },                          //
-    { day: 1, month: 6, isLunar: false, name: "Quốc tế Thiếu nhi", imagePrefix: "0601" },                                   //    
+    { day: 19, month: 5, isLunar: false, name: "sinh Chủ tịch Hồ Chí Minh", imagePrefix: "0519" },                          //ok
+    { day: 1, month: 6, isLunar: false, name: "Quốc tế Thiếu nhi", imagePrefix: "0601" },                                   //ok   
     { day: 27, month: 7, isLunar: false, name: "Thương binh liệt sỹ", imagePrefix: "0727" },                                //ok   
 
     { day: 15, month: 8, isLunar: true, name: "Tết Trung Thu", imagePrefix: "0815" },   // ÂM LỊCH                                //ok    
 
-    { day: 2, month: 9, isLunar: false, name: "Quốc khánh Việt Nam", imagePrefix: "0902" },                                 //
+    { day: 2, month: 9, isLunar: false, name: "Quốc khánh Việt Nam", imagePrefix: "0902" },                                 //ok
     { day: 20, month: 10, isLunar: false, name: "Phụ nữ Việt Nam", imagePrefix: "1020" },                                   //ok
-    { day: 9, month: 11, isLunar: false, name: " Quốc tế Nam giới", imagePrefix: "1109" },                      // chưa có ảnh
+    { day: 9, month: 11, isLunar: false, name: " Quốc tế Nam giới", imagePrefix: "1109" },                                  //ok
     { day: 20, month: 11, isLunar: false, name: "Nhà giáo Việt Nam", imagePrefix: "1120" },                                 //ok
-    { day: 24, month: 11, isLunar: false, name: "Văn hóa Việt Nam", imagePrefix: "1124" },                      // chưa có ảnh
+    { day: 24, month: 11, isLunar: false, name: "Văn hóa Việt Nam", imagePrefix: "1124" },                                  //ok
     { day: 22, month: 12, isLunar: false, name: "Thành Lập Quân Đội Nhân Dân Việt Nam", imagePrefix: "1222" },              //ok
-    { day: 24, month: 12, isLunar: false, name: "Lễ Giáng Sinh", imagePrefix: "1224" },                                     //
+    { day: 24, month: 12, isLunar: false, name: "Lễ Giáng Sinh", imagePrefix: "1224" },                                     //ok
     
     
     
